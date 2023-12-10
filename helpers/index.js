54dcc8e26c63e743fa4a81c33e716d2a7d4bb8ff
Mykola-Ts <tsybulskiyk@gmail.com>
2023-12-10 21:17:58 +0200

    completed task hw06-email

--- helpers/index.js
+++ helpers/index.js
@@ -1,3 +1,3 @@
-export { HttpError } from "./httpError.js";
+export { HttpError } from "./HttpError.js";
 export { sendEmail } from "./sendEmail.js";
 export { sendVerifyEmail } from "./sendVerifyEmail.js";
